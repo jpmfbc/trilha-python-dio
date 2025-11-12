402103504b0bbbcce1f0f3ddfca2720daf458587
jpmfbc <jpmfbc@gmail.com>
2025-11-11 21:29:57 -0300

    função nova conta, listar contas, novo usuário e listar usuários

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -1,8 +1,13 @@
+
 menu = """
 
 [d] Depositar
 [s] Sacar
 [e] Extrato
+[c] Novo usuário
+[l] Listar usuário
+[n] Nova conta
+[u] Listar contas
 [q] Sair
 
 => """
@@ -12,6 +17,11 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+usuarios =[{}]
+usuarios.clear()
+contas =[{}]
+contas.clear()
 
 def saque(*,saldo,valor,extrato,limite,numero_saques,limite_saques):
     excedeu_saldo = valor > saldo
@@ -56,6 +66,70 @@ def deposito(saldo,valor,extrato,/):
 
     return saldo,extrato
 
+def novo_usuario(nome,data,cpf,endereco,/):
+    jaCadastrado = False
+    for usuario in usuarios:
+        if(str(usuario.get("cpf")) == str(cpf)):
+           print("usuário não cadastrado, pois cpf já foi cadastrado")
+           jaCadastrado = True
+           break
+    
+    if(jaCadastrado == False):
+        usuario = {"nome": f"{nome}", "cpf": f"{cpf}", "data": f"{data}","endereco": f"{endereco}"}
+        usuarios.append(usuario)
+        print("usuário cadastrado com sucesso")
+
+def listar_usuarios():
+    print(f"Usuários: \n")
+    if(len(usuarios) == 0):
+        print("nenhum usuário cadastrado")
+        return
+    
+    for usuario in usuarios:
+        print("nome: "+str(usuario.get("nome"))+
+              f"\nCpf: "+str (usuario.get("cpf"))+
+              f"\nData Nascimento :"+
+              str(usuario.get("data"))+
+              f"\nEndereco: "+
+              str(usuario.get("endereco")))
+
+def listar_contas():
+    print(f"Contas: \n")
+    if(len(contas) == 0):
+        print("nenhuma conta cadastrada")
+        return
+    
+    for conta in contas:
+        print("Agência: "+str(conta.get("agencia"))+
+              f"\nNúmero: "+str (conta.get("numero"))+
+              f"\nCpf :"+
+              f"\nUsuário :"+
+              dict(conta.get("usuario")).get("nome"))
+
+def nova_conta():
+  cpf = input("informe cpf do usuário para cadastrar conta:")
+  usuarioCadastrado = False
+  usuarioVincular = {}
+  for usuario in usuarios:
+      if(str(usuario.get("cpf")) == cpf):
+          usuarioCadastrado = True
+          usuarioVincular = usuario
+          break
+  if(usuarioCadastrado == False):
+      print("Conta não cadastrada pois não tem usuário com o cpf informado")
+      return
+  
+          
+  agencia = "0001"
+  numeroUltimo =0 
+  if(len(contas)>0):
+   numeroUltimo = int(contas[len(contas)-1].get("numero"))
+  numero = numeroUltimo+1
+  novaConta = {"agencia":f"{agencia}","numero":f"{numero}","usuario":usuarioVincular}
+  contas.append(novaConta)
+  print("Conta cadastrada com sucesso")
+  return
+
 while True:
 
     opcao = input(menu)
@@ -80,6 +154,52 @@ while True:
 
     elif opcao == "q":
         break
+    elif opcao == "c":
+        nome = str(input("informe nome do usuário:"))
+        data = str(input("informe data de nascimento (ex: 20/04/1987):"))
+        cpfInformado = str(input("informe cpf(apenas número):"))
+        logradouro = str(input("informe logradouro:"))
+        numero = str(input("informe número:"))
+        bairro = str(input("informe bairro:"))
+        cidade = str(input("informe cidade:"))
+        estado = str(input("informe estado(ex: PE):"))
 
+        validado = False
+        cpf = ""
+        for letra in cpfInformado:
+            if(letra.isdigit()):
+                cpf += letra
+        
+        while(validado == False):
+            
+            if(nome is not "" and
+               data is not "" and
+               cpf is not "" and 
+               logradouro is not "" and
+               bairro is not "" and
+               cidade is not "" and
+               estado is not ""):
+                validado = True
+            
+            if(validado == False):
+                print("faltou informa dados paracadastrar usuario, informe novamente")
+                nome = str(input("informe nome do usuário:"))
+                data = str(input("informe data de nascimento (ex: 20/04/1987):"))
+                cpf = str(input("informe cpf(apenas número):"))
+                logradouro = str(input("informe logradouro:"))
+                numero = str(input("informe número:"))
+                bairro = str(input("informe bairro:"))
+                cidade = str(input("informe cidade:"))
+                estado = str(input("informe estado(ex: PE):"))
+
+        endereco = f"{logradouro}, {numero} - {bairro} {cidade}/{estado}"
+        novo_usuario(nome,data,cpf,endereco)
+    elif opcao == "l":
+        listar_usuarios()
+    elif opcao == "u":
+        listar_contas()
+    elif opcao == "n":
+        nova_conta()
+         
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
